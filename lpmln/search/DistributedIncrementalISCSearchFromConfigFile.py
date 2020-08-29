91580704f67027996d1a7a965a0117bdb9287613
王彬 <wangbiu@foxmail.com>
2020-08-29 15:19:12 +0800

    添加分层搜索方法文件
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ lpmln/search/DistributedIncrementalISCSearchFromConfigFile.py
@@ -0,0 +1,13 @@
+
+# -*- encoding: utf-8 -*-
+"""
+@Time    : 2020-08-29 15:05
+@Author  : 王彬
+@Email   : wangbiu@foxmail.com
+@File    : DistributedIncrementalISCSearchFromConfigFile.py
+"""
+
+
+if __name__ == '__main__':
+    pass
+    
