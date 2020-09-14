032f22dca48e3c49ce47f9e7bb03246383a8214b
王彬 <wangbiu@foxmail.com>
2020-09-14 23:42:30 +0800

    暂存
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

--- lpmln/iset/misc/ISetRuleGenerator.py
+++ lpmln/iset/misc/ISetRuleGenerator.py
@@ -31,6 +31,32 @@ def generate_all_rules(max_iset_atom_size=1, is_use_extended_rules=False):
     return rules
 
 
+def generate_all_rules_iset_fmt(max_iset_atom_size=1, is_use_extended_rules=False):
+    rule_set_size = 3
+    if is_use_extended_rules:
+        rule_set_size = 4
+
+    iset_number = 2 ** rule_set_size - 1
+    counter = BaseCounter(iset_number, [max_iset_atom_size] * iset_number)
+    rules = list()
+    while True:
+        rule_idx = counter.get_current_indicator()
+        # print(rule_idx)
+        if rule_idx is None:
+            break
+
+        rule = list()
+        for i in range(iset_number):
+            iset = set()
+            for j in range(rule_idx[i]):
+                iset.add(i+1+j*iset_number)
+            rule.append(iset)
+
+        rules.append(rule)
+
+    return rules
+
+
 def construct_rule_from_isets_atom_sizes(rule_set_size, isets_atom_sizes):
     rule = list()
     for i in range(rule_set_size):
@@ -45,11 +71,27 @@ def construct_rule_from_isets_atom_sizes(rule_set_size, isets_atom_sizes):
     return rule
 
 
+def construct_rule_from_isets(rule_set_size, isets):
+    rule = list()
+    for i in range(rule_set_size):
+        rule.append(set())
+    iset_number = len(isets)
+    for iset_id in range(1, iset_number + 1):
+        composition = iscm.get_iset_binary_bits(iset_id, rule_set_size)
+        for i in range(rule_set_size):
+            if composition[i] == 1:
+                for j in isets[iset_id-1]:
+                    rule[i].add(j)
+    return rule
+
+
+
 if __name__ == '__main__':
-    rules = generate_all_rules(1, False)
+    # rules = generate_all_rules(1, False)
+    rules = generate_all_rules_iset_fmt(1, False)
     cnt = 0
     for r in rules:
         cnt += 1
-        print(cnt, " : ", r)
+        print(cnt, " : ", r, construct_rule_from_isets(3, r))
     pass
     
