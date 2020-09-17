3ce228a3ae5bbf7b39eda29cad270a0da47e2837
王彬 <wangbiu@foxmail.com>
2020-09-17 20:24:28 +0800

    减少ISetCondition中保存的数据量
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

--- lpmln/iset/ISetCondition.py
+++ lpmln/iset/ISetCondition.py
@@ -9,19 +9,34 @@
 
 
 class ISetCondition:
-    def __init__(self, icondition, singletons):
+    def __init__(self, icondition, singletons, is_save_mem=True):
         self.icondition = icondition
         self.singletom_iset_ids = singletons
         self.iset_number = len(icondition)
         self.ne_iset_ids = set()
         self.contain_se_valid_rules = False
+        self.is_save_mem = is_save_mem
         for i in range(self.iset_number):
             if self.icondition[i] == 1:
                 self.ne_iset_ids.add(i)
 
         self.ne_iset_number = len(self.ne_iset_ids)
+        if self.is_save_mem:
+            self.icondition = None
 
     def __str__(self):
+        ne_isets = [str(s) for s in self.ne_iset_ids]
+        if len(self.singletom_iset_ids) == 0:
+            return ",".join(ne_isets)
+        else:
+            singletons = [str(s) for s in self.singletom_iset_ids]
+            string = "%s:%s" % (",".join(ne_isets), ",".join(singletons))
+            return string
+
+    def stringify(self):
+        """
+        do not use this function
+        """
         icondition = [str(s) for s in self.icondition]
 
         if len(self.singletom_iset_ids) == 0:
@@ -30,6 +45,7 @@ class ISetCondition:
             singletons = [str(s) for s in self.singletom_iset_ids]
             condition = "%s:%s" % (",".join(icondition), ",".join(singletons))
             return condition
+
 
     def get_ne_iset_str(self):
         isets = [str(s) for s in self.ne_iset_ids]
