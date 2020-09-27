c528f394b76d80a8b43d045329c5bcc5d475fcae
王彬 <wangbiu@foxmail.com>
2020-09-27 23:31:38 +0800

    添加linecache测试代码
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

--- tests/lpmln/LineCacheTest.py
+++ tests/lpmln/LineCacheTest.py
@@ -11,7 +11,7 @@ import lpmln.iset.RawIConditionUtils as riu
 import linecache
 
 def test_line_cache():
-    data_file = riu.get_complete_raw_icondition_file(0, 1, 1, "lpmln", False)
+    data_file = riu.get_complete_raw_icondition_file(1, 1, 0, "lpmln", False)
     for i in range(0, 1200):
         print(i, linecache.getline(data_file, i))
 
